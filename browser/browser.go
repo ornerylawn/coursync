bcbfd82a6ed2eee790dfabe0beae5de2ded04158
Ryan Brown <ryan@ryanleebrown.com>
2013-06-30 13:29:16 -0700

    Sync working now. It's too slow though.

--- browser/browser.go
+++ browser/browser.go
@@ -1,8 +1,12 @@
+// Copyright 2013 Ryan Brown (ryan@ryanleebrown.com).
+
 // Package browser provides access to coursera.org videos for courses
 // in which you're enrolled.
 package browser
 
 import (
+	"encoding/json"
+	"errors"
 	"fmt"
 	"io"
 	"io/ioutil"
@@ -10,22 +14,59 @@ import (
 	"net/http"
 	"net/http/cookiejar"
 	"net/url"
+	"os"
+	"regexp"
 	"strings"
 	"time"
 )
 
+// A Browser makes requests to coursera, scraping for course
+// information and videos for the courses in which you are enrolled.
 type Browser struct {
-	Client *http.Client
-	CsrfToken string
+	Client     *http.Client
+	CsrfToken  string
+	User       User
+	SigninTime time.Time
+}
+
+// A User represents the person who is logged-in.
+type User struct {
+	FullName string `json:"full_name"`
+	Id       int
+}
+
+// A Topic is what we would think of as a "course" that may or may not
+// be scheduled (e.g. "Machine Learning").
+type Topic struct {
+	Name      string
+	ShortName string `json:"short_name"`
+	Courses   []*Course
+}
+
+// A Course is a scheduled instance of a Topic (e.g. "A 10 week
+// 'Machine Learning' course starting 4/22/2013")
+type Course struct {
+	Name     string
+	Active   bool
+	HomeLink string `json:"home_link"`
 }
 
 const (
-	userAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1500.63 Safari/537.36"
-	signinPageUrl = "https://www.coursera.org/account/signin"
-	signinUrl = "https://www.coursera.org/maestro/api/user/login"
-	courseAuthUrlFormat = "https://class.coursera.org/%s/auth/auth_redirector?type=login&subtype=normal"
-	videoUrlFormat = "https://class.coursera.org/%s/lecture/download.mp4?lecture_id=%s"
-	PauseDuration = 5 * time.Second
+	userAgent           = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1500.63 Safari/537.36"
+	signinPageUrl       = "https://www.coursera.org/account/signin"
+	signinUrl           = "https://www.coursera.org/maestro/api/user/login"
+	topicListUrlFormat  = "https://www.coursera.org/maestro/api/topic/list_my?user_id=%d"
+	courseAuthUrlFormat = "%sauth/auth_redirector?type=login&subtype=normal"
+	courseUrlFormat     = "%slecture/index"
+	videoUrlFormat      = "%slecture/download.mp4?lecture_id="
+)
+
+var (
+	// Controls the length of the pause before each request (to simulate
+	// human-like behavior).
+	PauseDuration = 3 * time.Second
+	// Returned from anything other than SignIn() if the session is expired.
+	ErrSessionExpired = errors.New("session expired")
 )
 
 // Create a new coursera browser.
@@ -44,10 +85,10 @@ func New() *Browser {
 // Sign into coursera. Must be called before anything else.
 func (b *Browser) SignIn(email, password string) error {
 	err := b.getSigninPage()
+	b.SigninTime = time.Now()
 	if err != nil {
 		return err
 	}
-	time.Sleep(PauseDuration)
 	err = b.signIn(email, password)
 	if err != nil {
 		return err
@@ -56,13 +97,15 @@ func (b *Browser) SignIn(email, password string) error {
 }
 
 func (b *Browser) newRequest(method, url string, body io.Reader) (*http.Request, error) {
+	// Simulate human-like behavior.
+	time.Sleep(PauseDuration)
 	req, err := http.NewRequest(method, url, body)
 	if err != nil {
 		return nil, err
 	}
 	if b.CsrfToken != "" {
 		req.AddCookie(&http.Cookie{
-			Name: "csrftoken",
+			Name:  "csrftoken",
 			Value: b.CsrfToken,
 		})
 	}
@@ -77,20 +120,13 @@ func (b *Browser) getSigninPage() error {
 	if err != nil {
 		return err
 	}
-	fmt.Println(req)
-	fmt.Println(b.Client.Jar)
 	resp, err := b.Client.Do(req)
 	if err != nil {
 		return err
 	}
 	defer resp.Body.Close()
-	bytes, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return err
-	}
-	fmt.Println(b.Client.Jar)
-	fmt.Println(string(bytes))
-	return nil
+	_, err = ioutil.ReadAll(resp.Body)
+	return err
 }
 
 // Signs into coursera. Must be called *after* `getSigninPage` so that
@@ -99,7 +135,7 @@ func (b *Browser) signIn(email, password string) error {
 	b.CsrfToken = makeCsrfToken()
 	formData := url.Values{
 		"email_address": {email},
-		"password": {password},
+		"password":      {password},
 	}
 	req, err := b.newRequest("POST", signinUrl, strings.NewReader(formData.Encode()))
 	if err != nil {
@@ -110,71 +146,151 @@ func (b *Browser) signIn(email, password string) error {
 	req.Header.Add("Referer", signinPageUrl)
 	req.Header.Add("X-CSRFToken", b.CsrfToken)
 	req.Header.Add("X-Requested-With", "XMLHttpRequest")
-	fmt.Println(req)
-	fmt.Println(b.Client.Jar)
 	resp, err := b.Client.Do(req)
 	if err != nil {
 		return err
 	}
 	defer resp.Body.Close()
-	bytes, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return err
-	}
-	fmt.Println(b.Client.Jar)
-	fmt.Println(string(bytes))
-	return nil
+	d := json.NewDecoder(resp.Body)
+	err = d.Decode(&b.User)
+	return err
 }
 
 func makeCsrfToken() string {
 	charset := "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYX"
-	token := "";
+	token := ""
 	for i := 0; i < 24; i++ {
 		j := rand.Intn(len(charset))
-		token += charset[j:j+1]
+		token += charset[j : j+1]
 	}
 	return token
 }
 
-func (b *Browser) GetCourseList() ([]string, error) {
-	return []string{"audiomusicengpart1-001"}, nil
+// Retrieves all of the topics in which the current user is enrolled.
+func (b *Browser) GetTopicList() ([]*Topic, error) {
+	if b.SessionExpired() {
+		return nil, ErrSessionExpired
+	}
+	req, err := b.newRequest("GET", fmt.Sprintf(topicListUrlFormat, b.User.Id), nil)
+	if err != nil {
+		return nil, err
+	}
+	resp, err := b.Client.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	topics := make([]*Topic, 0)
+	d := json.NewDecoder(resp.Body)
+	err = d.Decode(&topics)
+	return topics, err
 }
 
 // Obtains cookies needed to access a course.
-func (b *Browser) AuthCourse(course string) error {
-	req, err := b.newRequest("GET", fmt.Sprintf(courseAuthUrlFormat, course), nil)
+func (b *Browser) AuthCourse(course *Course) error {
+	if b.SessionExpired() {
+		return ErrSessionExpired
+	}
+	req, err := b.newRequest("GET", fmt.Sprintf(courseAuthUrlFormat, course.HomeLink), nil)
 	if err != nil {
 		return err
 	}
-	fmt.Println(req)
-	fmt.Println(b.Client.Jar)
 	resp, err := b.Client.Do(req)
 	if err != nil {
 		return err
 	}
 	defer resp.Body.Close()
-	bytes, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return err
+	_, err = ioutil.ReadAll(resp.Body)
+	return err
+}
+
+// Retrieves all of the video urls for the course.
+func (b *Browser) GetVideoUrlList(course *Course) ([]string, error) {
+	if b.SessionExpired() {
+		return nil, ErrSessionExpired
 	}
-	fmt.Println(b.Client.Jar)
-	fmt.Println(string(bytes))
-	return nil
-}
-
-func (b *Browser) GetVideoList(course string) ([]string, error) {
-	return []string{"25"}, nil
-}
-
-func (b *Browser) DownloadVideo(course, video string) (io.Reader, error) {
-	req, err := b.newRequest("GET", fmt.Sprintf(videoUrlFormat, course, video), nil)
+	// Get the HTML that has the video urls embeded.
+	req, err := b.newRequest("GET", fmt.Sprintf(courseUrlFormat, course.HomeLink), nil)
 	if err != nil {
 		return nil, err
 	}
-	fmt.Println(req)
 	resp, err := b.Client.Do(req)
 	if err != nil {
 		return nil, err
 	}
-	return resp.Body, nil
+	defer resp.Body.Close()
+	bytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	// Search the HTML for video urls.
+	re, err := regexp.Compile(regexp.QuoteMeta(fmt.Sprintf(videoUrlFormat, course.HomeLink)) + `\d*`)
+	if err != nil {
+		return nil, err
+	}
+	return re.FindAllString(string(bytes), -1), nil
+}
+
+// Downloads a video if a file with the same name doesn't already
+// exist in the directory. Creates the directory if it doesn't already
+// exist. Returns os.ErrExist if the file already exists.
+func (b *Browser) SyncVideo(dirname, videoUrl string) error {
+	if b.SessionExpired() {
+		return ErrSessionExpired
+	}
+	// Create the directory if it doesn't already exist.
+	err := os.Mkdir(dirname, 0777)
+	if err != nil && !os.IsExist(err) {
+		return err
+	}
+	// Start the download now in order to obtain the filename.
+	r, err := b.StartVideoDownload(videoUrl)
+	if err != nil {
+		return err
+	}
+	defer r.Body.Close()
+	// Parse filename.
+	disp, ok := r.Header[http.CanonicalHeaderKey("Content-Disposition")]
+	if !ok || len(disp) != 1 {
+		return errors.New("error parsing filename from response")
+	}
+	start := strings.Index(disp[0], `"`)
+	end := strings.LastIndex(disp[0], `"`)
+	if start == -1 || end == -1 {
+		return errors.New("error parsing filename from response")
+	}
+	basename := disp[0][start+1 : end]
+	// Check if file already exists.
+	filename := fmt.Sprintf("%s/%s", dirname, basename)
+	_, err = os.Stat(filename)
+	if err == nil {
+		return os.ErrExist
+	}
+	if !os.IsNotExist(err) {
+		return err
+	}
+	// File doesn't exist, continue with download.
+	f, err := os.OpenFile(filename, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0666)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	io.Copy(f, r.Body)
+	return nil
+}
+
+// Sends a request for the video and returns the *http.Response.
+func (b *Browser) StartVideoDownload(videoUrl string) (*http.Response, error) {
+	if b.SessionExpired() {
+		return nil, ErrSessionExpired
+	}
+	req, err := b.newRequest("GET", videoUrl, nil)
+	if err != nil {
+		return nil, err
+	}
+	return b.Client.Do(req)
+}
+
+func (b *Browser) SessionExpired() bool {
+	return b.SigninTime.Add(15 * time.Minute).Before(time.Now())
 }
--- coursync.go
+++ coursync.go
@@ -1,12 +1,13 @@
+// Copyright 2013 Ryan Brown (ryan@ryanleebrown.com)
+
 package main
 
 import (
+	"code.google.com/p/gopass"
 	"coursync/browser"
 	"fmt"
-	"code.google.com/p/gopass"
-	"io"
 	"os"
-	"time"
+	"os/signal"
 )
 
 func main() {
@@ -16,68 +17,100 @@ func main() {
 	fmt.Scan(&email)
 	password, err := gopass.GetPass("Password: ")
 	if err != nil {
-		panic(err)
+		fmt.Println(err)
+		return
 	}
 
-	fmt.Println("Signing in...")
+	// Allow user to ctrl+c.
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	go func(){
+		for sig := range c {
+			fmt.Println(sig)
+			os.Exit(1)
+		}
+	}()
 
+	fmt.Println("Signing in...")
 	b := browser.New()
 	err = b.SignIn(email, password)
 	if err != nil {
-		panic(err)
+		fmt.Println(err)
+		return
 	}
+	fmt.Printf("Welcome, %s!\n", b.User.FullName)
 
-	fmt.Println("Pausing for human-like behavior...")
-	time.Sleep(browser.PauseDuration)
-
-	fmt.Println("Obtaining your course list...")
-	courses, err := b.GetCourseList()
+	fmt.Println("Getting your course list...")
+	topics, err := b.GetTopicList()
 	if err != nil {
-		panic(err)
+		fmt.Println(err)
+		return
 	}
 
+	// Print topic/course list.
 	fmt.Println()
-	for i, v := range courses {
-		fmt.Printf("%d. %s\n", i+1, v)
+	for i, topic := range topics {
+		active := 0
+		for _, course := range topic.Courses {
+			if course.Active {
+				active++
+			}
+		}
+		fmt.Printf("%d. %s (%d of %d active)\n", i+1, topic.Name, active, len(topic.Courses))
 	}
 	fmt.Println()
 
-	for _, course := range courses {
-		fmt.Println("Pausing for human-like behavior...")
-		time.Sleep(browser.PauseDuration)
-
-		fmt.Printf("Signing into %s...\n", course)
-		err = b.AuthCourse(course)
-		if err != nil {
-			fmt.Println(err)
-			continue
-		}
-
-		fmt.Println("Pausing for human-like behavior...")
-		time.Sleep(browser.PauseDuration)
-		fmt.Printf("Obtaining video list for %s...\n", course)
-		videos, err := b.GetVideoList(course)
-		if err != nil {
-			fmt.Println(err)
-			continue
-		}
-
-		for i, video := range videos {
-			fmt.Println("Pausing for human-like behavior...")
-			time.Sleep(browser.PauseDuration)
-
-			fmt.Printf("Downloading video %d of %d...", i, len(videos))
-			r, err := b.DownloadVideo(course, video)
+	// Sync videos for each active course, two at a time.
+	done := make(chan int)
+	semaphore := make(chan int, 2)
+	semaphore <- 1
+	semaphore <- 1
+	workers := 0
+	for _, topic := range topics {
+		for i, course := range topic.Courses {
+			if !course.Active {
+				fmt.Printf("%s (%d) is not active.\n", topic.Name, i+1)
+				continue
+			}
+			fmt.Printf("Getting video list for %s (%d)...\n", topic.Name, i+1)
+			err = b.AuthCourse(course)
 			if err != nil {
 				fmt.Println(err)
 				continue
 			}
-			f, err := os.OpenFile(course + video + ".mp4", os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0666)
+			videos, err := b.GetVideoUrlList(course)
 			if err != nil {
 				fmt.Println(err)
 				continue
 			}
-			io.Copy(f, r)
+			dirname := fmt.Sprintf("%s-%s", topic.ShortName, course.Name)
+			for j, video := range videos {
+				workers++
+				go func(dirname, video string, n, j int) {
+					<-semaphore
+					defer func(){
+						semaphore <- 1
+						done <- 1
+					}()
+					fmt.Printf("Syncing video %d of %d...\n", j+1, n)
+					err := b.SyncVideo(dirname, video)
+					if err != nil {
+						if os.IsExist(err) {
+							fmt.Printf("(video %d already exists)\n", j+1)
+						} else {
+							fmt.Println(err)
+							return
+						}
+					}
+				}(dirname, video, len(videos), j)
+			}
 		}
 	}
+
+	// Wait for the workers to finish.
+	for i := 0; i < workers; i++ {
+		<-done
+	}
+
+	fmt.Println("\nEnjoy the gift of knowledge!")
 }
